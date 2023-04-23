3a35cdc65290ba2f4ceda476bda09904cea3b05d
Lucas Martins Romão <lmartins.romao@gmail.com>
2023-04-23 12:26:49 -0300

    Commit inicial

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const User = use("App/Models/User")
+
+class UserController {
+
+    async index(){
+        return User.all()
+    }
+
+    async show({params}){
+        const usuario = User.findOrFail(params.id)
+        return usuario
+    }
+
+    async store({request}){
+        const data = request.only([
+            'email',
+            'senha',
+            'save_data'
+        ])
+
+        const usuario = User.create(data)
+        return usuario
+    }
+
+    async update({params, request}){
+        const usuario = await User.findOrFail(params.id)
+        const data = request.only([
+            'email',
+            'senha',
+            'save_data'
+        ])
+        usuario.merge(data)
+        await usuario.save()
+        return usuario
+    }
+
+    async destroy({params}){
+        const usuario = await User.findOrFail(params.id)
+        return await usuario.delete()
+    }
+
+    async autenticar({request}){
+        const data = request.only([
+            'email',
+            'senha'
+        ])
+        return await User.autenticar(data)
+    }
+
+}
+
+module.exports = UserController
---
+++ database/migrations/1503250034279_user.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UserSchema extends Schema {
+  up () {
+    this.create('usuarios', (table) => {
+      table.increments().unsigned()
+      table.string('email', 256).notNullable().unique()
+      table.string('senha', 33).notNullable()
+      table.string('save_data', 256)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('usuarios')
+  }
+}
+
+module.exports = UserSchema
